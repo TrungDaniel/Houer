16c863716cac20a3a5eacdbf03886ccff2299edf
TrungDaniel <trungdaniel1998@gmail.com>
2020-02-26 22:53:03 +0700

    cập nhập giao diện

--- app/src/main/java/com/example/houer/ChooseLoginRegistationActivity.java
+++ app/src/main/java/com/example/houer/ChooseLoginRegistationActivity.java
@@ -4,17 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.Toast;
+
+import believe.cht.fadeintextview.TextViewListener;
 
 public class ChooseLoginRegistationActivity extends AppCompatActivity {
     private Button btnLogin, btnRegister;
+    private believe.cht.fadeintextview.TextView textView;
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_login_registation);
         init();
+        hienChu();
         // Màn hình khởi động đầu tiên
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
@@ -34,9 +43,24 @@ public class ChooseLoginRegistationActivity extends AppCompatActivity {
         });
     }
 
+    private void hienChu() {
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                textView.setLetterDuration(50); // sets letter duration programmatically
+                textView.setText("Kết nối và trao đi yêu thương đến \n mọi người "); // sets the text with animation (Read "KNOWN BUGS" if it doesn't give desired results)
+                textView.isAnimating(); // returns current boolean animation state (optional)
+            }
+        },1000);
+
+    }
+
     private void init() {
         btnLogin = findViewById(R.id.btn_login);
         btnRegister = findViewById(R.id.btn_register);
+        textView = findViewById(R.id.tv_welcome);
+        handler = new Handler();
 
     }
 }
